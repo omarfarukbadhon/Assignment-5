77e9a21490a2d43933ec3a91bead5d3d2ecd6a54
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-02-19 20:28:34 +0600

    after click once a button it's disable

--- js/colorChange.js
+++ js/colorChange.js
@@ -3,4 +3,5 @@ function selectSeat(selectSeatID){
     getSelectSeatID.classList.remove('bg-slate-200')
     getSelectSeatID.classList.add('bg-[#1DD100]')
     getSelectSeatID.classList.add('text-white')
+    getSelectSeatID.removeAttribute('onclick');
 }
